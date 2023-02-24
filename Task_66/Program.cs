17e4da8262a286ce8b0487a9015c68d366b84f7b
Marina Dianova <m_di@mail.ru>
2023-02-24 11:02:16 +0300

    Homework 9

---
+++ Task_66/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sum(int numM, int numN)
+{
+    if (numN < numM)
+    {
+        return numN;
+    }
+    return numN + Sum(numM, numN - 1);
+
+}
+
+int m = ReadInt("Введите M: ");
+int n = ReadInt("Введите N: ");
+
+if (n <= m)
+    System.Console.WriteLine($"Сумма натуральных чисел от {n} до {m} = {Sum(n, m)}");
+
+else
+    System.Console.WriteLine($"Сумма натуральных чисел от {m} до {n} = {Sum(m, n)}");
---
+++ Task_68/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Akerman(int numM, int numN)
+{
+    if (numM == 0)
+        return numN + 1;
+
+    else if (numM != 0 && numN == 0)
+        return Akerman(numM - 1, 1);
+
+    else
+        return Akerman(numM - 1, Akerman(numM, numN - 1));
+
+}
+
+metka:
+int m = ReadInt("Введите m: ");
+int n = ReadInt("Введите n: ");
+
+if (m < 0 || n < 0)
+{
+    System.Console.WriteLine("Числа не должны быть меньше нуля!");
+    goto metka;
+}
+
+System.Console.WriteLine($"A(m,n) = {Akerman(m, n)}");
